4008b2ca403b402528c1be84d90a0fdce75d3d00
Maciej <maciey138@gmail.com>
2025-11-13 23:25:55 +0100

    add todos

--- generate.py
+++ generate.py
@@ -148,3 +148,12 @@ if __name__ == "__main__":
                 pass
     finally:
         keyboard.unhook_all()
+
+# TODO: check Chaos Bolt, Clone, Conjure Giant, Control Flames, Creation, Divine Word, Doom of Stacked Stones, Greater Restoration, Guardian of Nature
+# TODO: fix damage type coloring on Tasha's Otherworldly Guise text
+# TODO: fix Arcane Lock attr labels overlaps (why do they even happen?)
+# TODO: Lunar Transfer i Octarine Spray font size
+# TODO: Lunar Transfer table
+# TODO: better contrast damage type colors
+# TODO: no dmg type coloring in Animate Objects
+# TODO: Greater Restoration tablelist should not move; also the last row isn't parsed, treated as normal text
